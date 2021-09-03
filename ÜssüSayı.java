71da5d716ea99ee842e7fb83d814c1e2984ef816
selmankayapa <80275310+selmankayapa@users.noreply.github.com>
2021-09-03 14:44:47 +0300

    Add files via upload

---
+++ ÜssüSayı.java
@@ -0,0 +1,23 @@
+package üssü.sayı;
+import java.util.Scanner;
+public class ÜssüSayı {
+
+    public static void main(String[] args) {
+        int sayı;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("sayı giriniz : ");
+        sayı=input.nextInt();
+        
+        for(int i=1;i<=sayı;i*=4){
+            System.out.println(i);
+        }
+        for(int z=1;z<=sayı;z*=5){
+            System.out.println(z);
+        }
+        }
+        }
+        
+    
+    
+
